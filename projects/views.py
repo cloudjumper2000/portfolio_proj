edcc55db61b6d9b21dfcdd0f283af5cc6e252448
Cloud User <centos@ip-10-0-0-247.us-west-2.compute.internal>
2018-08-01 23:32:42 +0000

    Home Page working

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Project
+
 def home(request):
-    return render(request, 'projects/home.html')
+    projects = Project.objects
+    return render(request, 'projects/home.html', {'projects':projects})
